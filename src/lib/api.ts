9ddf64dd85af3dfb4cc95d6a42a3af0b42b762dd
hicham <relaxingchannel65@gmail.com>
2026-05-22 17:20:04 +0100

    first commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const UNAUTHORIZED_EVENT = "stallion:unauthorized";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: `${import.meta.env.VITE_API_URL ?? ""}/api`,
   headers: { "Content-Type": "application/json" },
 });
 
