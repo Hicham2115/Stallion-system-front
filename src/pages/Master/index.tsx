ca2d9417dd31766344a84c366bee73b7d997ba3b
hicham <relaxingchannel65@gmail.com>
2026-05-25 12:13:37 +0100

    first commit

--- src/pages/Master/index.tsx
+++ src/pages/Master/index.tsx
@@ -180,7 +180,7 @@ function AgencyCard({ agency }: { agency: AgencyRow }) {
                           </div>
                         )}
                         <span className="font-medium text-white">{m.name}</span>
-                        {m.isCloser && <PackageCheck className="w-3 h-3 text-amber-400 flex-shrink-0" title="Closer" />}
+                        {m.isCloser && <PackageCheck className="w-3 h-3 text-amber-400 flex-shrink-0" aria-label="Closer" />}
                       </div>
                     </td>
                     <td className="px-4 py-2.5">
